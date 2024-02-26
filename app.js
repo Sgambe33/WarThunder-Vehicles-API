a6951ff850e9d4bcc5d75b8476344d4d860991ff
Sgambe33 <80386324+Sgambe33@users.noreply.github.com>
2024-02-26 22:06:03 +0100

    Refactored code, Mongoose removed

--- app.js
+++ app.js
@@ -1,43 +1,22 @@
 require('dotenv').config();
 const cors = require('cors');
-const fs = require(`fs`);
+const fs = require('fs');
 const path = require('path');
 const express = require('express');
-const mongoose = require('mongoose');
-
-mongoose.connect(process.env.DATABASE_URL_PROD);
-const database = mongoose.connection;
-
-database.on('error', (error) => {
-    console.log(error)
-})
-
-database.once('connected', () => {
-    console.log('Database Connected');
-})
 
 const app = express();
-app.use(cors())
+app.use(cors());
 app.use(express.json());
-app.set('json spaces', 2)
+app.set('json spaces', 2);
 
-var module_files = fs.readdirSync(path.resolve(__dirname, `routes`), {
-    withFileTypes: true,
-});
-
-module_files.forEach((file) => {
-    if (!file.name.endsWith(`.js`)) {
-        return;
+fs.readdirSync(path.resolve(__dirname, 'routes')).forEach(file => {
+    if (file.endsWith('.js')) {
+        const handler_file = require(path.join(__dirname, 'routes', file));
+        app.use(`/api${handler_file.base_route}`, cors(), handler_file.handler());
     }
-    let handler_file = require(path.join(__dirname, `routes`, file.name).replace(`.js`, ``));
-    let route = handler_file.base_route;
-    console.log(handler_file)
-    let handler = handler_file.handler();
-
-    app.use(`/api${route}`, cors(), handler);
 });
 
-app.use("/assets", express.static('assets'));
+app.use('/assets', express.static('assets'));
 
 app.use('*', (req, res) => {
     res.json({
@@ -46,10 +25,10 @@ app.use('*', (req, res) => {
         hostname: req.hostname,
         query: req.query,
         params: req.params
-    })
-        .end()
-})
+    });
+});
 
-app.listen(3000, () => {
-    console.log(`Server Started at http://localhost:${3000}`)
-})
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server Started at http://localhost:${PORT}`);
+});
