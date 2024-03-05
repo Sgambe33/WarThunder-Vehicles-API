6f46a1ba6631cd3d0ef324400861daa2c50eb6b0
Sgambe33 <80386324+Sgambe33@users.noreply.github.com>
2024-03-05 22:53:04 +0100

    Regex for version validation

--- utils/constants.js
+++ utils/constants.js
@@ -1,9 +1,13 @@
+const {Vehicle} = require("../models/models");
 const UNIT_TYPES = {
     'tanks': ['lighttank', 'mediumtank', 'heavytank', 'tankdestroyer', 'spaa'],
     'aircrafts': ['fighter', 'stormovik', 'bomber', 'divebomber'],
     'ships': ['torpedoboat', 'submarinechaser', 'minelayer', 'transport', 'navalferrybarge', 'destroyer', 'torpedogunboat', 'ship']
 }
 
+const VERSION_REGEX = /^\d+\.\d+\.\d+\.\d+$/;
+
 module.exports = {
-    UNIT_TYPES
+    UNIT_TYPES,
+    VERSION_REGEX
 }
