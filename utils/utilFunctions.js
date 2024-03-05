5b5d7e8273a239e97de04be02d8913c533239357
Sgambe33 <80386324+Sgambe33@users.noreply.github.com>
2024-03-05 22:53:52 +0100

    Latest version getter + image appending

--- utils/utilFunctions.js
+++ utils/utilFunctions.js
@@ -1,9 +1,27 @@
+const {Vehicle} = require('../models/models');
+
 function exclude(obj, keys) {
     return Object.fromEntries(
-      Object.entries(obj).filter(([key]) => !keys.includes(key))
+        Object.entries(obj).filter(([key]) => !keys.includes(key))
     );
-  }
+};
+
+async function latestVersion(){
+    const randomVehicle = await Vehicle.findOne();
+    return randomVehicle.dataValues.version;
+}
+
+function appendImages(queryResult, req){
+    if (queryResult === null) return null;
+    queryResult.dataValues.images = {
+        image: `${req.get('host')}/assets/images/${queryResult.dataValues.identifier.toLowerCase()}.png`,
+        techtree: `${req.get('host')}/assets/techtrees/${queryResult.dataValues.identifier.toLowerCase()}.png`
+    };
+    return queryResult;
+}
 
 module.exports = {
-    exclude
+    exclude,
+    latestVersion,
+    appendImages
 }
