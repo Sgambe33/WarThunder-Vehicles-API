9789b21613ff5bf7a85a82f3134fd364b36d83a2
Sgambe33 <80386324+Sgambe33@users.noreply.github.com>
2024-05-14 18:16:29 +0200

    New killstreak filter

--- routes/vehicles.js
+++ routes/vehicles.js
@@ -1,25 +1,31 @@
 require('dotenv').config();
 const express = require('express');
-const {Vehicle} = require('../models/models');
-const {appendImages} = require("../utils/utilFunctions");
+const { Vehicle } = require('../models/models');
+const { appendImages } = require("../utils/utilFunctions");
+const { Op } = require('sequelize');
 
 module.exports = {
-    base_route: '/vehicles', handler: () => {
-        const route = express.Router({caseSensitive: false});
+    base_route: '/vehicles',
+    handler: () => {
+        const route = express.Router({ caseSensitive: false });
 
         route.get('', async (req, res) => {
+            const { limit = 50, page = 0, country, type, era, isPremium, isGift, killstreak } = req.query;
+            const user_limit = Math.min(limit, 300);
+
+            const filter = {};
+            if (country) filter.country = country;
+            if (type) filter.vehicle_type = type;
+            if (era) filter.era = era;
+            if (isPremium) filter.is_premium = isPremium;
+            if (isGift) filter.is_gift = isGift;
+
+            const identifierFilters = [];
+            if (!killstreak) identifierFilters.push({ [Op.notLike]: '%killstreak' });
+            if (identifierFilters.length > 0) filter.identifier = { [Op.and]: identifierFilters };
+
+            const attributes = ['identifier', 'country', 'vehicle_type', 'era', 'arcade_br', 'realistic_br', 'simulator_br', 'event', 'release_date', 'is_premium', 'is_gift', 'value', 'req_exp', 'ge_cost'];
             try {
-                const {limit = 50, page = 0, country, type, era, isPremium, isGift} = req.query;
-                const user_limit = Math.min(limit, 200);
-
-                const filter = {};
-                if (country) filter.country = country;
-                if (type) filter.vehicle_type = type;
-                if (era) filter.era = era;
-                if (isPremium) filter.is_premium = isPremium;
-                if (isGift) filter.is_gift = isGift;
-
-                const attributes = ['identifier', 'country', 'vehicle_type', 'era', 'arcade_br', 'realistic_br', 'simulator_br', 'event', 'release_date', 'is_premium', 'is_gift', 'value', 'req_exp', 'ge_cost'];
                 const vehicles = await Vehicle.findAll({
                     where: filter,
                     limit: user_limit,
@@ -30,10 +36,9 @@ module.exports = {
                 const vehiclesWithImages = vehicles.map(v => appendImages(v, req));
                 res.status(200).json(vehiclesWithImages);
             } catch (err) {
-                res.status(500).json({error: err.message});
+                res.status(500).json({ error: err.message });
             }
         });
         return route;
     }
 };
-
