702e2ecb4edd357255f2b6b08b4fc868d81399f0
Sgambe33 <80386324+Sgambe33@users.noreply.github.com>
2024-08-13 01:54:41 +0200

    Migration to postgres

--- utils/db.js
+++ utils/db.js
@@ -1,16 +1,25 @@
 const {Sequelize} = require('sequelize');
 const db = {};
 const sequelize = new Sequelize({
-        storage: './utils/database.sqlite3',
-        dialect: 'sqlite',
-        logging: console.log,
-        pool: {
-            max: 5,
-            min: 0,
-            acquire: 30000,
-            idle: 10000
+    database: process.env.DATABASE,
+    username: process.env.PG_USER,
+    password: process.env.PG_PWD,
+    host: process.env.PG_HOST,
+    port: 5432,
+    dialect: 'postgres',
+    logging: console.log,
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    },dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
         }
-    })
+    }
+})
 
 db.sequelize = sequelize
 db.Sequelize = Sequelize
