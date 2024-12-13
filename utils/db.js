c83801f752f1cd71b82431e91e8be91610bb0296
Sgambe33 <80386324+Sgambe33@users.noreply.github.com>
2024-12-13 11:03:57 +0100

    Moved back to sqlite3

--- utils/db.js
+++ utils/db.js
@@ -1,24 +1,9 @@
 const {Sequelize} = require('sequelize');
 const db = {};
 const sequelize = new Sequelize({
-    database: process.env.DATABASE,
-    username: process.env.PG_USER,
-    password: process.env.PG_PWD,
-    host: process.env.PG_HOST,
-    port: 5432,
-    dialect: 'postgres',
+    dialect: 'sqlite',
     logging: console.log,
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    },dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false
-        }
-    }
+    storage: './utils/vehiclesdb.sqlite3',
 })
 
 db.sequelize = sequelize
