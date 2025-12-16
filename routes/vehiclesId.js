10ad96b787247a8016c3b75adcb877f9138aba78
Sgambe33 <80386324+Sgambe33@users.noreply.github.com>
2025-12-16 17:15:33 +0100

    Vehicle history is with query

--- routes/vehiclesId.js
+++ routes/vehiclesId.js
@@ -1,36 +1,69 @@
-require('dotenv').config();
 const express = require('express');
-const {Vehicle, VehicleOld} = require('../models/models');
-const {Op} = require("sequelize");
-const {appendImages} = require("../utils/utilFunctions");
+const { Vehicle, VehicleOld } = require('../models/models');
+const { Op } = require("sequelize");
+const { appendImages, latestVersion } = require("../utils/utilFunctions");
+const { VERSION_REGEX } = require("../utils/constants");
 
-const findVehicle = async (model, id) => {
-    return await model.findOne({
-        where: {identifier: {[Op.like]: id}}
-    });
-}
+const findVehicle = (model, id, version = null) => {
+    const where = { identifier: { [Op.like]: id } };
+    if (version) where.version = version;
+    return model.findOne({ where });
+};
 
 module.exports = {
-    base_route: '/vehicles', handler: () => {
-        const route = express.Router({caseSensitive: false});
+    base_route: '/vehicles',
+    handler: () => {
+        const route = express.Router({ caseSensitive: false });
 
         route.get('/:id', async (req, res) => {
-            const {id} = req.params;
+            const { id } = req.params;
+            const { version: targetVersion } = req.query;
+
             try {
-                let queryResult = await findVehicle(Vehicle, id);
-                const versions = await VehicleOld.findAll({
-                    attributes: ['version'], where: {identifier: {[Op.like]: id}}
-                });
-                if (queryResult) {
-                    queryResult.dataValues.versions = versions.map(v => v.dataValues.version);
-                    queryResult.dataValues.versions.push(queryResult.dataValues.version);
-                    queryResult = appendImages(queryResult, req);
+                let result = null;
+
+                if (targetVersion) {
+                    if (!VERSION_REGEX.test(targetVersion)) {
+                        return res.status(400).json({ error: 'Invalid version provided' });
+                    }
+
+                    const currentLiveVersion = await latestVersion();
+
+                    if (targetVersion === currentLiveVersion) {
+                        result = await findVehicle(Vehicle, id);
+                    } else {
+                        result = await findVehicle(VehicleOld, id, targetVersion);
+                    }
+
+                } else {
+                    const [vehicle, oldVersions] = await Promise.all([
+                        findVehicle(Vehicle, id),
+                        VehicleOld.findAll({
+                            attributes: ['version'],
+                            where: { identifier: { [Op.like]: id } }
+                        })
+                    ]);
+
+                    if (vehicle) {
+                        result = vehicle;
+                        const history = oldVersions.map(v => v.version);
+                        result.dataValues.versions = [...history, vehicle.version].sort();
+                    }
                 }
-                res.status(queryResult ? 200 : 404).json(queryResult || {error: 'Vehicle not found'});
+
+                if (result) {
+                    result = appendImages(result, req);
+                    return res.status(200).json(result);
+                } else {
+                    return res.status(404).json({ error: 'Vehicle not found' });
+                }
+
             } catch (err) {
-                res.status(500).json({error: err.message});
+                console.error(err);
+                return res.status(500).json({ error: err.message });
             }
         });
+
         return route;
     }
 };
