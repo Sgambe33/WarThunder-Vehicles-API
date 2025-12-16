a3343a6310d6d5c6ade4c246b6557a691b88a6cb
Sgambe33 <80386324+Sgambe33@users.noreply.github.com>
2025-12-16 17:12:33 +0100

    latestVersion was flawed

--- utils/utilFunctions.js
+++ utils/utilFunctions.js
@@ -1,11 +1,19 @@
-const {Vehicle} = require('../models/models');
+const { Vehicle } = require('../models/models');
 
-async function latestVersion(){
-    const randomVehicle = await Vehicle.findOne();
-    return randomVehicle.dataValues.version;
+async function latestVersion() {
+    const versions = await Vehicle.findAll({
+        attributes: ['version'],
+        group: ['version']
+    });
+
+    const sortedVersions = versions
+        .map(v => v.version)
+        .sort((a, b) => a.localeCompare(b, undefined, { numeric: true }));
+
+    return sortedVersions[sortedVersions.length - 1];
 }
 
-function appendImages(queryResult, req){
+function appendImages(queryResult, req) {
     if (queryResult === null) return null;
     queryResult.dataValues.images = {
         image: `https://${req.get('host')}/assets/images/${queryResult.dataValues.identifier.toLowerCase()}.png`,
